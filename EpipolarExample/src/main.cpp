4f6499118f59e5b598500cfa0aaf4fc589199150
guhwan.bae <allofdocuments@gmail.com>
2017-12-26 04:52:43 +0900

    Find epipole line

---
+++ EpipolarExample/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2017. 12. 25.
+ *      Author: ghbae
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+	//drawEpipolarLines("EpipolarLines_WINDOW", F, left, right, ptr_img_left, ptr_img_right);
+
+	return 0;
+}
